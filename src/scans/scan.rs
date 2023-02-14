d412c02638aaaa780daac4b9be8175050e5a949c
Mick de Peinder <mickdep@live.nl>
2023-02-14 15:53:19 +0100

    Nuclei scan is integrated as well.

--- src/scans/scan.rs
+++ src/scans/scan.rs
@@ -7,7 +7,7 @@ pub trait Scan {
     //This is awesome lol. This is called an "Associated type" and it functions as a polymorphic type.
     //Each struct that implements this trait must define this type. The scan "NmapScan" can for example define the following:
     //  type ScanResult = NmapScanResult;
-    //While the scan "FeroxbusterScan" can define the following:
+//While the scan "FeroxbusterScan" can define the following:
     //  type ScanResult = FeroxbusterScanResult;
     //This allows me to define a polymorphic return type.
     //https://doc.rust-lang.org/rust-by-example/generics/assoc_items/types.html
